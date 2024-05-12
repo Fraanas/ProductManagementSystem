b5073f27b875ca45fd5f727ce7fb05dbf6fd8609
Fraanas <franek.biskup@pakunova.pl>
2024-05-12 13:03:48 +0200

    Pierwszy commit

--- src/main/java/com/uep/wap/model/Product.java
+++ src/main/java/com/uep/wap/model/Product.java
@@ -1,34 +1,72 @@
 package com.uep.wap.model;
-
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name="Products")
-public class Product{
+@Table(name = "Product")
+public class Product {
     @Id
-    @Column(name ="id")
-    private char id;
-    @Column(name ="name")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    //ManyToMany relations with Order
+    @ManyToMany(mappedBy = "products", fetch = FetchType.EAGER)
+    private List<Order> orders = new ArrayList<>();
+
+    //ManyToMany relationship with CustomerHistory
+    @ManyToMany(mappedBy = "products", fetch = FetchType.LAZY)
+    private List<CustomerHistory> customerHistories;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255)")
     private String name;
-    @Column(name ="price")
-    private double  price;
-
-    @Column(name = "availability")
+    @Column(columnDefinition = "DECIMAL(10,2) DEFAULT 0.00")
+    private BigDecimal price;
+    @Column(nullable = false)
     private boolean availability;
-
-    @Column(name = "description")
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    public void setId(char id){
-        this.id = id;
-    }
-    public char getId(){
-        return id;
-    }
-    public Product(){
+    // Many-to-Many relationship with Warehouse
+    @ManyToMany(mappedBy = "products", fetch = FetchType.LAZY)
+    private List<Warehouse> warehouses = new ArrayList<>();
 
+    //ManyToOne relationship with Supplier
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    private Supplier supplier;
+
+
+    //ManyToOne relationship with Product Category
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    //gettery i settery
+    public Long getProductId() {
+        return productId;
     }
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<CustomerHistory> getCustomerHistories() {
+        return customerHistories;
+    }
+
+    public void setCustomerHistories(List<CustomerHistory> customerHistories) {
+        this.customerHistories = customerHistories;
+    }
 
     public String getName() {
         return name;
@@ -38,23 +76,52 @@ public class Product{
         this.name = name;
     }
 
-    public double getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(Integer points) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
-    public boolean getAvailability(){return availability; }
+    public boolean isAvailability() {
+        return availability;
+    }
 
-    public void setAvailability(boolean availability) {this.availability = availability; }
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
 
-    public String getDescription (){return description; }
+    public String getDescription() {
+        return description;
+    }
 
-    public void setDescription (String description) {this.description = description; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public Product(String name, Integer points){
+    public List<Warehouse> getWarehouses() {
+        return warehouses;
+    }
 
+    public void setWarehouses(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
     }
 }
+
---
+++ src/main/java/com/uep/wap/model/Return.java
@@ -0,0 +1,67 @@
+package com.uep.wap.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "Return")
+public class Return{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long returnId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date returnDate;
+
+    public enum ReturnStatus {
+        RECEIVED, AUTHORIZED, REFUND, CLOSED
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Return.ReturnStatus status;
+
+    //getters and setters
+    public Long getReturnId() {
+        return returnId;
+    }
+
+    public void setReturnId(Long returnId) {
+        this.returnId = returnId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public ReturnStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReturnStatus status) {
+        this.status = status;
+    }
+}
+
+
+
