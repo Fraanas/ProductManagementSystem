3a960a867a47a6ec55229e6ab71c41debafb638f
Fraanas <franek.biskup@pakunova.pl>
2024-05-28 22:11:42 +0200

    Back-end of system

---
+++ src/main/java/com/uep/wap/dto/SupplierDTO.java
@@ -0,0 +1,52 @@
+package com.uep.wap.dto;
+
+import java.util.List;
+
+public class SupplierDTO {
+    private Long supplierId;
+    private String companyName;
+    private String contactName;
+    private String phoneNumber;
+    private List<Long> productIds;
+
+    // Getters and Setters
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Long> getProductIds() {
+        return productIds;
+    }
+
+    public void setProductIds(List<Long> productIds) {
+        this.productIds = productIds;
+    }
+}
